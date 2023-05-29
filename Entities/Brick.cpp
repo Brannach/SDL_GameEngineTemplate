ac049f915f835891f7292b9565acfb4fc00fe7cc
Marcelo Gomes <brannach@gmail.com>
2023-05-29 16:23:23 -0300

    Bricks from map now collide with ball!

--- Entities/Brick.cpp
+++ Entities/Brick.cpp
@@ -5,9 +5,13 @@ void Brick::Update(float delta)
 
 }
 
-void Brick::Draw2()
+void Brick::Draw()
 {
-	Box box = ActorCollider->Get();
-	SDL_SetRenderDrawColor(Engine::GetInstance()->GetRenderer(), BrickColor.r, BrickColor.g, BrickColor.b, SDL_ALPHA_OPAQUE);
-	SDL_RenderFillRect(Engine::GetInstance()->GetRenderer(), &box);
+	if (BrickColor.a != 0)
+	{
+		Box box = ActorCollider->Get();
+		SDL_SetRenderDrawColor(Engine::GetInstance()->GetRenderer(), BrickColor.r, BrickColor.g, BrickColor.b, SDL_ALPHA_OPAQUE);
+		SDL_RenderFillRect(Engine::GetInstance()->GetRenderer(), &box);
+	}
+	else Actor::Draw();
 }
--- Entities/Brick.h
+++ Entities/Brick.h
@@ -9,13 +9,14 @@ class Brick : public Actor
 {
 public:
 	Brick();
+	Brick(Properties* pr) : Actor(pr) { BrickColor = { 0,0,0,0 }; };
 	Brick(Properties* pr, SDL_Color brickColor) : Actor(pr) 
 	{
 		BrickColor = brickColor;
 	};
 
 	void Update(float delta);
-	void Draw2();
+	void Draw();
 
 protected:
 	SDL_Color BrickColor;
